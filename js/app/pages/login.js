0b70e0c05364413af2aefc2f9c4adbe3959d3469
KVISHIK <syperdan1090@gmail.com>
2026-01-21 14:30:00 +0100

    Add files via upload

---
+++ js/app/pages/login.js
@@ -0,0 +1,10 @@
+export const login = {
+    template: `
+        <section class="auth-page">
+            <div class="auth-box">
+                <span class="auth-title">Login page</span>
+                <p class="auth-text">Test component</p>
+            </div>
+        </section>
+    `
+};
